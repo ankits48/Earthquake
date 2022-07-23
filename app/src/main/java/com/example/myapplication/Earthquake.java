ebf3a373768bab8e2832dcf7ef5aad8216998541
Ankit Sharma <ankitsharma29july@gmail.com>
2022-07-24 01:43:35 +0530

    add project

---
+++ app/src/main/java/com/example/myapplication/Earthquake.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+public class Earthquake {
+    private double magnitude;
+    private String location;
+    private long date;
+    private String url;
+
+    public Earthquake(double magnitude, String location, long date,String url) {
+        this.magnitude = magnitude;
+        this.location = location;
+        this.date = date;
+        this.url =url;
+    }
+
+    public double getMagnitude() {
+        return magnitude;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
+
+
